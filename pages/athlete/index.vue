7ce2ce465e88378199db657c08823e86459f2e0b
sara galvan <sara.galvan@udea.edu.co>
2024-11-06 00:00:35 -0500

    Listado de atleta. key. loading.

--- pages/athlete/index.vue
+++ pages/athlete/index.vue
@@ -1,9 +1,10 @@
 <script setup>
-    const { status, data } = await useMacsApi('/athlete')
-    const headers = [
-        { title: "Id", value: "id"},
-        { title: "Nombre", value: "name"},
-        { title: "Apellido", value: "lastName"},
+    import { ref } from 'vue'
+    const { status, data, error } = await useMacsApi('/athlete')
+    const headers = ref([
+        { title: "Id", key: "id"},
+        { title: "Nombre", key: "name"},
+        { title: "Apellido", key: "lastName"},
         {
             title: "Identificación",
             align: "center",
@@ -13,27 +14,31 @@
             ]
             
         },
-        { title: 'Pais de nacimiento', value: "homeland.name" },
+        { title: 'Pais de nacimiento', key: "homeland.name" },
         { title: 'Cinturon', key: 'belt.color' },
         { title: "Peso", key: 'weight'},
         { title: "Sexo", key: 'sex'},
         { title: "Fecha de nacimiento", key: "birthDate"}
-    ]
+    ])
+    function isPending() {
+        return status === 'pending'
+    }
 </script>
 <template>
-    <div v-if="status === 'pending'">
-        Cargando ...
-    </div>
-    <div v-else>
-        <v-data-table
-            :items="data"
-            item-key="id"
-            items-per-page="10"
-            :headers="headers"
-        >
-        <template v-slot:item.sex="{ value }">
-            {{ value === 'MALE' ? "Masculino": "Femenino" }}
-        </template>
-    </v-data-table>
-    </div>
+<div v-if="status === 'error'">
+    Carga de atletas fallida. {{ error }}
+</div>
+<div v-else>
+    <v-data-table
+        :items="data"
+        item-key="id"
+        items-per-page="10"
+        :headers="headers"
+        :loading=isPending()
+    >
+    <template v-slot:item.sex="{ value }">
+        {{ value === 'MALE' ? "Masculino": "Femenino" }}
+    </template>
+</v-data-table>
+</div>
 </template>
