a834874809b36f763184b0a474979477a7e4c1b2
sara galvan <sara.galvan@udea.edu.co>
2024-11-05 23:10:03 -0500

    editing athletes table

--- pages/athlete/index.vue
+++ pages/athlete/index.vue
@@ -1,7 +1,24 @@
 <script setup>
     const { status, data } = await useMacsApi('/athlete')
-
-    
+    const headers = [
+        { title: "Id", value: "id"},
+        { title: "Nombre", value: "name"},
+        { title: "Apellido", value: "lastName"},
+        {
+            title: "Identificación",
+            align: "center",
+            children: [
+                { title: "Numero", key: "idNumber"},
+                { title: "Tipo", key: "documentType.name"}
+            ]
+            
+        },
+        { title: 'Pais de nacimiento', value: "homeland.name" },
+        { title: 'Cinturon', key: 'belt.color' },
+        { title: "Peso", key: 'weight'},
+        { title: "Sexo", key: 'sex'},
+        { title: "Fecha de nacimiento", key: "birthDate"}
+    ]
 </script>
 <template>
     <div v-if="status === 'pending'">
@@ -12,6 +29,11 @@
             :items="data"
             item-key="id"
             items-per-page="10"
-        ></v-data-table>
+            :headers="headers"
+        >
+        <template v-slot:item.sex="{ value }">
+            {{ value === 'MALE' ? "Masculino": "Femenino" }}
+        </template>
+    </v-data-table>
     </div>
 </template>
