61479af1819d09158ac86c8ffea7ac8446acc4a3
Sara Galvan <sara.galvan@udea.edu.co>
2025-02-03 01:03:52 -0500

    Fetch on load

--- pages/pyramid.vue
+++ pages/pyramid.vue
@@ -1,4 +1,8 @@
 <template>
+  <div v-if="!rounds" >
+    No se encontraron combates para la categoría seleccionada
+  </div>
+  <template v-else>
     <TournamentBracket
       :rounds="rounds"
       @onMatchClick="onMatchClick"
@@ -6,10 +10,14 @@
     />
   </template>
   
-  <script lang="ts" setup>
+</template>
+
+<script lang="ts" setup>
   import { TournamentBracket } from "vue3-tournament"
   import type IRound from "vue3-tournament/interface/IRound"
   import "vue3-tournament/style.css"
+  const { fetcher } = useMacsApi();
+  const category = useState();
   
   const onMatchClick = (matchId: string | number): void => {
     alert(`click: ${matchId}`)
@@ -20,72 +28,7 @@
     console.log("match", match)
   }
   
-  const rounds = ref<IRound[]>([
-  {
-    "matchs": [
-      {
-        "title": "combate 1",
-        "id": "match1",
-        "winner": "1",
-        "team1": {
-          "id": "1",
-          "name": "Competitor 1",
-          "score": 2
-        },
-        "team2": {
-          "id": "2",
-          "name": "Competitor 2",
-          "score": 1
-        }
-      },
-      {
-        "title": "paso directo",
-        "id": "match2",
-        "feedIn": {
-          "id": "3",
-          "name": "Competitor 3"
-        }
-      }
-    ]
-  },
-  {
-    "matchs": [
-      {
-        "title": "combate 2",
-        "id": "match3",
-        "team1": {
-          "id": "1",
-          "name": "Competitor 1"
-        },
-        "team2": {
-          "id": "3",
-          "name": "Competitor 3"
-        }
-      },
-      {
-        "id": "match4",
-        "feedIn": {
-          "id": "4",
-          "name": "Competitor 4"
-        }
-      }
-    ]
-  },
-  {
-    "matchs": [
-      {
-        "id": "any_match_id",
-        title: "combate 3",
-        "team1": {
-            "id": "",
-            "name": "Combate 2 **"
-        },
-        "team2": {
-          "id": "4",
-          "name": "Competidor 4",
-        }
-      }
-    ]
-  }
-])
+  const { data: rounds } = useAsyncData('categories', () =>
+    fetcher('/pyramid/2')
+  );
   </script>
